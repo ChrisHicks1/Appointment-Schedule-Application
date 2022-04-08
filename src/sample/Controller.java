85bf1ef47606ddc45c9a24131f06d67fba0c8465
ChrisHicks <chic118@wgu.edu>
2022-04-08 08:44:38 -0700

    Fourth Save

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,7 +9,6 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        System.out.println("hello");
     }
-
-    System.out.println("hello")
 }
