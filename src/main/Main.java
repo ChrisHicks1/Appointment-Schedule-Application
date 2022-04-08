dd3a9263fba40e89e99aea1fcbe96ae9453a66c3
ChrisHicks <chic118@wgu.edu>
2022-04-08 09:13:06 -0700

    Starting Save

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import helper.JDBC;
 import javafx.application.Application;
@@ -7,11 +7,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
+public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
@@ -20,11 +20,14 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        JDBC.openConnection();
+        launch(args);
 
 
-        JDBC.closeConnection();
+      //  JDBC.openConnection();
 
-        //launch(args);
+
+       // JDBC.closeConnection();
+
+
     }
 }
