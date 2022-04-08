6a48aa12d3df2551f639be9ee6bedaf4ace55dcf
ChrisHicks <chic118@wgu.edu>
2022-04-08 10:18:08 -0700

    More Language properties

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class Main extends Application{
@@ -17,6 +18,7 @@ public class Main extends Application{
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 450, 450));
         primaryStage.show();
+
     }
 
 
@@ -24,6 +26,11 @@ public class Main extends Application{
         Locale.setDefault(new Locale("fr"));
         launch(args);
 
+        ResourceBundle rb = ResourceBundle.getBundle("/main/Nat", Locale.getDefault());
+
+        if(Locale.getDefault().getLanguage().equals("es") || Locale.getDefault().getLanguage().equals("fr")){
+            System.out.println(rb.getString("hello") + " " + rb.getString("world"));
+        }
 
         JDBC.openConnection();
         JDBC.closeConnection();
