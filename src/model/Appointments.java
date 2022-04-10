4b3c6a9da783206127919b4905fce23bf829d9df
ChrisHicks <chic118@wgu.edu>
2022-04-10 09:13:30 -0700

    customer and appointment classes

--- src/model/Appointments.java
+++ src/model/Appointments.java
@@ -1,4 +1,103 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Appointments {
+    private int appointmentId;
+    private String title;
+    private String description;
+    private String type;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int customerId;
+    private int userId;
+    private int contactId;
+    private String contactName;
+
+    public Appointments(int appointmentId, String title, String description, String type, LocalDateTime startTime, LocalDateTime endTime, int customerId, int userId, int contactId, String contactName){
+        this.appointmentId = appointmentId;
+        this.title = title;
+        this.description = description;
+        this. type = type;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.customerId = customerId;
+        this.userId = userId;
+        this.contactId = contactId;
+        this.contactName = contactName;
+    }
+
+    public int getAppointmentId(){return appointmentId;}
+    public void setAppointmentId(){this.appointmentId = appointmentId;}
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getDescription(){return description;}
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(){
+        this.customerId = customerId;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+    public int getContactId() {
+        return contactId;
+    }
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+
+    public String getContactName() {
+        return contactName;
+    }
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
 }
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,27 +1,61 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Customer {
-  /*  private int id;
-    private String name;
-    private String address1;
-    private String address2;
+    private int customerId;
+    private String customerName;
+    private String address;
     private int postal;
-    private ? state;
-    private ? country;
-    private int number;
-    public Customer(int id, String address1, String address2, int postal, ? state, ? country, int number){
-        this.id = id;
-        this.address1 = address1;
-        this.address2 = address2;
+    private String phone;
+    private String country;
+    private String division;
+    private int divisionId;
+
+    public Customer(int customerId, String customerName, String address, int postal, String phone, String country, String division, int divisionId){
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
         this.postal = postal;
-        this.state = state;
+        this.phone = phone;
         this.country = country;
-        this.number = number;
-    } */
+        this.division = division;
+        this.divisionId = divisionId;
+    }
 
 
+    public int getCustomerId(){return customerId;}
 
+    public void setCustomerId(int customerId){this.customerId = customerId;}
 
+    public String getCustomerName(){return customerName;}
+
+    public void setCustomerName(String customerName){this.customerName = customerName;}
+
+    public String getAddress(){return address;}
+
+    public void setAddress(String address){this.address = address;}
+
+    public int getPostal(){return postal;}
+
+    public void setPostal(int postal){this.postal = postal;}
+
+    public String getPhone(){return phone;}
+
+    public void setPhone(String phone){this.phone = phone;}
+
+    public String getCountry(){return country;}
+
+    public void setCountry(String country){this.country = country;}
+
+    public String getDivision(){return division;}
+
+    public void setDivision(String division){this.division = division;}
+
+    public int getDivisionId(){return divisionId;}
+
+    public void setDivisionId(int divisionId){this.divisionId = divisionId;}
 
 
 }
