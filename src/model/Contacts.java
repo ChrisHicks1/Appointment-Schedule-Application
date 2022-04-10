909146b4c0163957af22cb52ce9750578a58daf7
ChrisHicks <chic118@wgu.edu>
2022-04-10 07:03:41 -0700

    started customer class

---
+++ src/model/Contacts.java
@@ -0,0 +1,9 @@
+package model;
+
+import javafx.collections.ObservableList;
+
+public class Contacts {
+
+
+
+}
---
+++ src/model/Customer.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String address1;
+    private String address2;
+    private int postal;
+    private ? state;
+    private ? country;
+    private int number;
+    public Customer(int id, String address1, String address2, int postal, ? state, ? country, int number){
+        this.id = id;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.postal = postal;
+        this.state = state;
+        this.country = country;
+        this.number = number;
+    }
+
+
+
+
+
+
+}
